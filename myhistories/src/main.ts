7034d2b6ab5eba5d54b432938b3435af3e8a5f85
npujol <naivy.luna@gmail.com>
2020-07-03 20:25:15 +0200

    Add auth check

--- myhistories/src/main.ts
+++ myhistories/src/main.ts
@@ -4,7 +4,24 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import { CHECK_AUTH } from "./store/actions.type.js";
+import ApiService from "./common/api.service.js";
+import DateFilter from "./common/date.filter.js";
+import ErrorFilter from "./common/error.filter.js";
+
 Vue.config.productionTip = false;
+Vue.filter("date", DateFilter);
+Vue.filter("error", ErrorFilter);
+
+ApiService.init();
+
+// Ensure we checked auth before each page load.
+// router.beforeEach((to, from, next) => {
+//   store.dispatch(CHECK_AUTH).then(next);
+// });
+
+router.beforeEach((to, from, next) =>
+  Promise.all([store.dispatch(CHECK_AUTH)]).then(next));
 
 new Vue({
   router,
