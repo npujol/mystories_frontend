63b4e5d5cedc5b9fd5c5fa505679143be652aba8
npujol <naivy.luna@gmail.com>
2020-07-06 18:52:09 +0200

    Update store dependencies

--- myhistories/src/store/index.ts
+++ myhistories/src/store/index.ts
@@ -1,11 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import home from "./home.module";
+import auth from "./auth.module";
+import history from "./history.module";
+import profile from "./profile.module";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules: {
+    home,
+    auth,
+    history,
+    profile
+  }
 });
