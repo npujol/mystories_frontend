b20948a353f85305c9fba7093f08d9105e58873b
npujol <naivy.luna@gmail.com>
2020-07-06 18:54:44 +0200

    Update main without apiClient

--- myhistories/src/main.ts
+++ myhistories/src/main.ts
@@ -5,7 +5,6 @@ import router from "./router";
 import store from "./store";
 
 import { CHECK_AUTH } from "./store/actions.type.js";
-import ApiService from "./common/api.service.js";
 import DateFilter from "./common/date.filter.js";
 import ErrorFilter from "./common/error.filter.js";
 
@@ -13,15 +12,10 @@ Vue.config.productionTip = false;
 Vue.filter("date", DateFilter);
 Vue.filter("error", ErrorFilter);
 
-ApiService.init();
-
 // Ensure we checked auth before each page load.
-// router.beforeEach((to, from, next) => {
-//   store.dispatch(CHECK_AUTH).then(next);
-// });
-
 router.beforeEach((to, from, next) =>
-  Promise.all([store.dispatch(CHECK_AUTH)]).then(next));
+  Promise.all([store.dispatch(CHECK_AUTH)]).then(next)
+);
 
 new Vue({
   router,
