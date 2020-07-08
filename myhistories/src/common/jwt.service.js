b0d1153617cb9f10f988879ede20f9aa9e026a9b
npujol <naivy.luna@gmail.com>
2020-07-08 16:33:42 +0200

    Add username to localstorage

--- myhistories/src/common/jwt.service.js
+++ myhistories/src/common/jwt.service.js
@@ -1,15 +1,36 @@
+import { ApiClient } from "../client";
+
 const ID_TOKEN_KEY = "id_token";
+const USERNAME = "username";
 
 export const getToken = () => {
   return window.localStorage.getItem(ID_TOKEN_KEY);
 };
 
-export const saveToken = token => {
+export const getUsername = () => {
+  return window.localStorage.getItem(USERNAME);
+};
+
+export const saveCredentials = (username, token) => {
+  window.localStorage.setItem(USERNAME, username);
   window.localStorage.setItem(ID_TOKEN_KEY, token);
 };
 
-export const destroyToken = () => {
+export const destroyCredentials = () => {
   window.localStorage.removeItem(ID_TOKEN_KEY);
+  window.localStorage.removeItem(USERNAME);
 };
 
-export default { getToken, saveToken, destroyToken };
+export const setHeader = () => {
+  const apiClient = ApiClient.instance;
+  apiClient.authentications.api_key.apiKeyPrefix = "Bearer";
+  apiClient.authentications.api_key.apiKey = getToken();
+};
+
+export default {
+  getToken,
+  getUsername,
+  saveCredentials,
+  destroyCredentials,
+  setHeader
+};
