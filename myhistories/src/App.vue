5080fc7b3c2ed8e0191e22eb0f7b2027bddd6203
npujol <naivy.luna@gmail.com>
2020-07-16 18:13:13 +0200

    Update app base

--- myhistories/src/App.vue
+++ myhistories/src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
     <RwvHeader />
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div> -->
-    <router-view></router-view>
-    <RwvFooter />
+
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col class="shrink">
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn :href="source" icon large target="_blank" v-on="on">
+                  <!-- <v-icon>mdi-code-tags</v-icon> -->
+                </v-btn>
+              </template>
+              <span>Source</span>
+            </v-tooltip>
+          </v-col>
+        </v-row>
+        <router-view></router-view>
+      </v-container>
+      <RwvFooter />
+    </v-main>
   </div>
 </template>
 
@@ -16,11 +29,18 @@ import RwvFooter from "./components/TheFooter.vue";
 
 export default {
   name: "App",
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  }),
   components: {
     RwvHeader,
     RwvFooter
+  },
+  created() {
+    this.$vuetify.theme.dark = true;
   }
 };
 </script>
-
-<style></style>
