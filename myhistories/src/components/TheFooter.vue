746fb2b4cf8c3dfd8d23215f2cacb1d212a36e65
npujol <naivy.luna@gmail.com>
2020-07-16 15:17:24 +0200

    Update footer

--- myhistories/src/components/TheFooter.vue
+++ myhistories/src/components/TheFooter.vue
@@ -1,14 +1,14 @@
 <template>
-  <footer class="footer">
-    <div class="content has-text-centered">
-      <p>
-        <router-link class="logo-font" :to="{ name: 'home', params: {} }"
-          >Histories</router-link
-        >
-        by <a href="https://github.com/npujol">Naivy Pujol Méndez</a>
-      </p>
-    </div>
-  </footer>
+  <v-footer class="footer">
+    <span>&copy; {{ new Date().getFullYear() }}</span>
+    <hr />
+    <p>
+      <router-link class="logo-font" :to="{ name: 'home', params: {} }"
+        >Histories</router-link
+      >
+      by <a href="https://github.com/npujol">Naivy Pujol Méndez</a>
+    </p>
+  </v-footer>
 </template>
 
 <script>
