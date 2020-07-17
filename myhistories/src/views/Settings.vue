e06e851bdc2fdafed1586d0b45585e0695f0db8f
npujol <naivy.luna@gmail.com>
2020-07-17 20:42:25 +0200

    Update setting with vuetify

--- myhistories/src/views/Settings.vue
+++ myhistories/src/views/Settings.vue
@@ -1,70 +1,52 @@
 <template>
-  <section>
-    <div>
-      <h1>Your Settings</h1>
-      <hr />
+  <v-card class="elevation-12 d-flex pa-2">
+    <v-card-title class="headline" dark>Your Settings</v-card-title>
 
-      <form @submit.prevent="updateSettings()">
-        <section>
-          <field class="file">
-            <div
-              class="image-preview"
-              v-if="currentUser.profile.image.length > 0"
-            >
-              <figure class="image is-128x128">
-                <img class="is-rounded" :src="currentUser.profile.image" />
-              </figure>
-            </div>
-            <hr />
-            <!-- <upload
-              v-model="currentUser.profile.image"
-              class="button is-primary"
-              accept="image/*"
-            >
-              <icon icon="upload"></icon>
-            </upload> -->
-            <input
-              type="file"
-              icon="upload"
-              class="button is-primary"
-              @change.native="previewImage"
-              accept="image/*"
-            />
-          </field>
-          <field label="Email">
-            <input
-              type="email"
-              v-model="currentUser.email"
-              placeholder="Email"
-              maxlength="30"
-            />
-          </field>
+    <v-card-text>
+      <v-form>
+        <v-img height="200" width="200" :src="currentUser.profile.image">
+        </v-img>
+        <v-file-input
+          :rules="[rules.photo]"
+          name="avatar"
+          accept="image/png, image/jpeg, image/bmp"
+          placeholder="Pick an avatar"
+          prepend-icon="mdi-camera"
+          label="Avatar"
+          hide-input
+          @change="previewImage"
+        >
+        </v-file-input>
 
-          <field label="Username">
-            <input
-              v-model="currentUser.profile.username"
-              placeholder="Username"
-              maxlength="30"
-            />
-          </field>
-
-          <field label="Bio">
-            <input
-              maxlength="200"
-              type="textarea"
-              v-model="currentUser.profile.bio"
-              placeholder="Short bio about you"
-            />
-          </field>
-          <field>
-            <button native-type="submit" class="button is-primary">
-              Update
-            </button>
-          </field>
-        </section>
-      </form>
-    </div>
-  </section>
+        <v-text-field
+          label="Username"
+          name="username"
+          prepend-icon="mdi-account"
+          type="text"
+          v-model="currentUser.username"
+          disabled
+        ></v-text-field>
+        <v-text-field
+          label="Email"
+          name="Email"
+          v-model="currentUser.email"
+          prepend-icon="mdi-mail"
+          type="text"
+          disabled
+        ></v-text-field>
+        <v-textarea
+          auto-grow
+          label="Bio"
+          rows="5"
+          v-model="currentUser.profile.bio"
+        ></v-textarea>
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn class="mr-4" @click="updateSettings()">Update</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
@@ -73,6 +55,15 @@ import { LOGOUT, UPDATE_USER } from "../store/actions.type.js";
 
 export default {
   name: "RwvSettings",
+  data() {
+    return {
+      valid: false,
+      rules: {
+        photo: v =>
+          !v || v.size < 2000000 || "Avatar size should be less than 2 MB!"
+      }
+    };
+  },
   computed: {
     ...mapGetters(["currentUser"])
   },
@@ -82,20 +73,12 @@ export default {
         this.$router.push({ name: "home" });
       });
     },
-    logout() {
-      this.$store.dispatch(LOGOUT).then(() => {
-        this.$router.push({ name: "home" });
-      });
-    },
-    previewImage(event) {
-      var input = event.target;
-      if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = e => {
-          this.currentUser.profile.image = e.target.result;
-        };
-        reader.readAsDataURL(input.files[0]);
-      }
+    previewImage(file) {
+      var reader = new FileReader();
+      reader.onload = e => {
+        this.currentUser.profile.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
     }
   }
 };
