246c2af87f8ed5fa5f1375ad033281cd87225f18
npujol <naivy.luna@gmail.com>
2020-10-08 18:19:58 +0200

    Add remove notification

--- mystories/src/components/VNotification.vue
+++ mystories/src/components/VNotification.vue
@@ -1,24 +1,40 @@
 <template>
-  <v-card outlined>
+  <v-card outlined grow class="pa-2">
     <div>
-      <v-row class="pa-2" justify="center">
-        <v-alert prominent text type="info">
-          <v-row class="flex mb-2" align="center">
-            <v-col class="flex mb-2">
+      <v-row justify="center">
+        <v-alert
+          :icon="isOpenIcon"
+          :color="isOpenColor"
+          grow
+          prominent
+          text
+          style="min-width: 100px; max-width: 95%;"
+          class="flex-grow-1 flex-shrink-0 pa-2"
+        >
+          <v-row align="center">
+            <v-col>
               {{ message.title }}
             </v-col>
-            <v-col class="shrink">
-              <v-btn color="primary" class="ma-2" dark @click="dialog = true">
-                Show
+            <v-row class="shrink pa-4">
+              <v-btn color="sucess" icon @click="dialog = true">
+                <v-icon> mdi-eye </v-icon>
               </v-btn>
-            </v-col>
+              <v-btn
+                color="error"
+                :disabled="inProgress"
+                icon
+                @click="destroy(message.pk)"
+              >
+                <v-icon> mdi-delete</v-icon>
+              </v-btn>
+            </v-row>
           </v-row>
         </v-alert>
 
         <v-dialog v-model="dialog" max-width="500px">
-          <v-card class="mx-auto" color="primary" dark max-width="400">
+          <v-card class="mx-auto" color="primary" dark max-width="500">
             <v-card-title>
-              <v-icon large left>
+              <v-icon medium left>
                 mdi-bell
               </v-icon>
               <span class="title font-weight-light">Notification</span>
@@ -49,15 +65,17 @@
                     class="subheading mr-1"
                     small
                     text
-                    @click="dialog = false"
+                    icon
+                    @click="toggleMessage(opened)"
                   >
                     <v-icon>
-                      mdi-email-open
+                      {{ isOpenIcon }}
                     </v-icon>
                   </v-btn>
                   <v-btn
                     class="subheading mr-1"
                     text
+                    icon
                     small
                     @click="dialog = false"
                   >
@@ -76,10 +94,14 @@
 </template>
 
 <script>
+import { OPEN_MESSAGE, MESSAGE_DESTROY } from "../store/actions.type.js";
+
 export default {
   data() {
     return {
-      dialog: false
+      dialog: false,
+      opened: false,
+      inProgress: false
     };
   },
   props: {
@@ -89,12 +111,32 @@ export default {
       default: null
     }
   },
+  mounted() {
+    if (this.message) {
+      this.opened = this.message.opened;
+    }
+  },
   computed: {
-    openMessage() {
-      return this.message.status === "opened" ? "mdi-email-open" : "mdi-email";
+    isOpenIcon() {
+      return this.opened === true ? "mdi-email-open" : "mdi-email";
     },
-    color() {
-      return this.message.status === "opened" ? "secondary" : "info";
+    isOpenColor() {
+      return this.opened === true ? "secondary" : "info";
+    }
+  },
+  methods: {
+    async toggleMessage(value) {
+      this.opened = await this.$store.dispatch(OPEN_MESSAGE, { value });
+    },
+    async destroy(pk) {
+      try {
+        this.inProgress = true;
+        await this.$store.dispatch(MESSAGE_DESTROY, { pk });
+        this.inProgress = false;
+        this.$router.go();
+      } catch (err) {
+        console.error(err);
+      }
     }
   }
 };
