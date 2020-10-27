e1d8a77a930a609a7cb93f40c1bd6ef0395e0cee
npujol <naivy.luna@gmail.com>
2020-10-27 15:58:05 +0100

    Update new story route

--- mystories/src/router/index.ts
+++ mystories/src/router/index.ts
@@ -79,7 +79,7 @@ const routes: Array<RouteConfig> = [
   },
   {
     name: "story-new",
-    path: "/story-new",
+    path: "/stories/new",
     props: true,
     component: () => import("../views/StoryNew.vue")
   },
